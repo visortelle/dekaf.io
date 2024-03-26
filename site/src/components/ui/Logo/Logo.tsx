da46187c73d3f74d54517067113e949cbbb3836f
visortelle <visortelle@gmail.com>
2024-03-26 08:45:21 +0400

    upd

--- site/src/components/ui/Logo/Logo.tsx
+++ site/src/components/ui/Logo/Logo.tsx
@@ -9,9 +9,9 @@ const Logo: React.FC<LogoProps> = (props) => {
     <div className={s.Logo}>
       <img className={s.LogoImage} src="/img/logo.png" />
       <div className={s.Text}>
-        <div className={s.Beta}>
+        {/* <div className={s.Beta}>
           <a target="_blank" href='https://teal.tools'><Beta text='by Teal Tools'/></a>
-        </div>
+        </div> */}
         <span className={s.TextDekaf}>dekaf</span>
         {/* <span className={s.TextForPulsar}>for Apache Pulsar</span> */}
       </div>
