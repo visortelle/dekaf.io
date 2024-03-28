d705bf8fd15f6afa374a429d01a729f95f95ea45
visortelle <visortelle@gmail.com>
2024-03-28 22:27:15 +0400

    upd

--- site/src/components/pages/HomePage/LinksBlock/LinksBlock.tsx
+++ site/src/components/pages/HomePage/LinksBlock/LinksBlock.tsx
@@ -9,7 +9,7 @@ const LinksBlock: React.FC = () => {
         <a href="/docs/quick-start">
           <Button type="primary" text='Start Free' onClick={() => { }} />
         </a>
-        <a href="https://demo.deka.io" target="_blank" style={{ display: 'inline-flex', flexDirection: 'column', gap: '0.25rem'}}>
+        <a href="https://demo.dekaf.io" target="_blank" style={{ display: 'inline-flex', flexDirection: 'column', gap: '0.25rem'}}>
           <Button type="regular" text="Live Demo" onClick={() => { }} />
         </a>
         {/* TODO - replace ask question with "Book Demo" after we'll have a demo :) */}
